655c578f767b38638f83e3bd2d80fd8200d39f3b
undefined8 <dzamilatskiy2019@bklawtech.com>
2017-01-10 22:55:03 +0000

    mad libzzzzz

---
+++ script.js
@@ -0,0 +1,15 @@
+var madLib = {
+    animal: "walrus",
+    things: "cookies",
+    feeling: "AAAAA",
+    profession: "Advance Memer",
+    clothing: "a rag",
+    things2: "pedistals",
+    person: "Adam Sandler",
+    place: "Ninja Turtle HQ",
+    verbIng: "rethinking",
+    food: "pineapple under the sea"
+}
+
+var story = "Say '"+madLib.food+"' the photographer said as the camera flashed! "+madLib.person+" and I had gone to "+madLib.place+" to get our photos taken today. The first photo we really wanted was a picture of us dressed as "+madLib.animal+" pretending to be a "+madLib.profession+". When we saw the proofs of it, I was a bit "+madLib.feeling+" because it looked different than I had imagined it would. (I hadn't imagined so many "+madLib.things+" behind us.) However, the second photo was exactly what I wanted. We both looked like "+madLib.things2+" wearing "+madLib.clothing+" and "+madLib.verbIng+", which was exactly what I had in mind!";
+console.log(story);
